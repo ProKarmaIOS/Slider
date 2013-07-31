c4e5a43589e0e664711076b00bfca0ac0220c34e
ProKarmaIOS <prokarmaios@outlook.com>
2013-07-31 14:49:19 -0600

    renamed a few vars

--- Slider/Slider/Scroller.h
+++ Slider/Slider/Scroller.h
@@ -14,13 +14,16 @@
 #define kScrollDistancePixels 10.0
 
 @interface Scroller : UIScrollView <UIScrollViewDelegate>
+{
+    int _selectedCover;
+}
 
-@property (nonatomic, strong) NSMutableArray *covers;
+@property (nonatomic, strong) NSMutableArray *deck;
 @property (nonatomic, strong) CAScrollLayer *animatedScrollLayer;
 @property (nonatomic, assign) int coverHeight;
 @property (nonatomic, assign) int coverWidth;
 
--(id)initWithFrame:(struct CGRect)frame covers:(NSMutableArray*)covers;
+-(id)initWithFrame:(struct CGRect)frame covers:(NSMutableArray*)deck;
 -(void)layoutLayer:(CAScrollLayer*)layer;
 
 @end
--- Slider/Slider/Scroller.m
+++ Slider/Slider/Scroller.m
@@ -10,11 +10,11 @@
 
 @implementation Scroller
 
--(id)initWithFrame:(struct CGRect)frame covers:(NSMutableArray *)covers
+-(id)initWithFrame:(struct CGRect)frame covers:(NSMutableArray *)deck
 {
     self = [super initWithFrame:frame];
     if (self) {
-        _covers = covers;
+        _deck = deck;
         
         //set properties of uiscrollview
         self.showsVerticalScrollIndicator = NO;
@@ -31,23 +31,67 @@
         _animatedScrollLayer.frame = frame;
         
         //add images to the CAScrollLayer
-        for (int i = 0; i < [_covers count]; ++i) {
-            UIImageView *cover = [[UIImageView alloc] initWithImage:[_covers objectAtIndex:i]];
-            [_animatedScrollLayer addSublayer:cover.layer];
+        for (int i = 0; i < [_deck count]; ++i) {
+            UIImageView *card = [[UIImageView alloc] initWithImage:[_deck objectAtIndex:i]];
+            [_animatedScrollLayer addSublayer:card.layer];
         }
         
         //frame height + size of all colapsed menu items
-        int contentHeight = frame.size.height + ( kScrollDistancePixels * ([_covers count] - 1) );
+        int contentHeight = frame.size.height + ( kScrollDistancePixels * ([_deck count] - 1) );
         
         self.contentSize = CGSizeMake(frame.size.width, contentHeight);
         
         [self.layer addSublayer:_animatedScrollLayer];
+        [self layoutLayer:_animatedScrollLayer];
     }
     return self;
 }
 
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // the amount scrolled in the scrollView
+    _selectedCover = (int)roundf(self.contentOffset.y/kScrollDistancePixels);
+    
+    // check if end is reached
+    if ( _selectedCover > ([_deck count] - 4) ) {
+        _selectedCover = [_deck count] - 4;
+    } else { // less than four cards
+        _selectedCover = 0;
+    }
+    
+    // update the CAScrollView based on how much the scroll view has scrolled
+    [self layoutLayer:_animatedScrollLayer];
+}
+
+//mutator for set selected cover
+-(void)setSelectedCover:(int)index
+{
+    //set the current selection to the index provided
+    if ( index != _selectedCover && index < ([_deck count] - 4) ) {
+        _selectedCover = index;
+        
+        [self layoutLayer:_animatedScrollLayer];
+        
+        self.contentOffset = CGPointMake(self.contentOffset.x, _selectedCover * kScrollDistancePixels);
+        
+    } else {
+        [self layoutLayer:_animatedScrollLayer];
+        
+        self.contentOffset = CGPointMake(self.contentOffset.x, _selectedCover * kScrollDistancePixels);
+        
+        _selectedCover = 0;
+    }
+}
+
+//accessor for selected cover
+-(int)getSelectedCover{
+    return _selectedCover;
+}
+
 -(void)layoutLayer:(CAScrollLayer*)layer
 {
+    CALayer *card;
+    NSArray *sublayerArray;
     
 }
 
